e43e96f4236a0dd90990e8e0c873bf09dcf705e7
Brooks Patton <brooks@brookzerker.com>
2018-04-30 15:37:30 -0600

    set up first page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <header>
+          <h1>Galvanize Student Notes</h1>
+        </header>
+        <main>
+          <h2>Login with GitHub</h2>
+        </main>
       </div>
     );
   }
